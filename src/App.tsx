2bc9d5dce39ed8daa6864c11eea01873485a1d29
Yitzhak <itzhakg@ravtech.co.il>
2022-03-23 23:18:34 +0200

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className="header">
+          Weather App
+        </div>
+        <main className="main">
+        
+            
+            <div className="weather">
+                <div className="weather-city">
+                    <span className="weather-city__title">
+                        <span className="weather-city__name">
+                        New York,
+                    </span>
+                    <span className="weather-city__region">
+                        New York
+                    </span>
+                    </span>
+                    
+                </div>
+                <div className="weather-forecast">
+                    <div className="forecast">
+                        <div className="day-header">
+                            <div className="day-header__title">
+                                Today
+                            </div>
+                            <div className="day-header__icon">
+                                <img src="" alt="icon"/>
+                            </div>
+                            
+                        </div>
+                        <div className="day-data">
+                            <div className="data-row">
+                                <div className="data-row__name">
+                                
+                                </div>
+                                <div className="data-row__value">
+                                
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="forecast">day2</div>
+                    <div className="forecast">day3</div>
+                    <div className="forecast">day4</div>
+                    <div className="forecast">day5</div>
+                </div>
+            
+            </div>
+        </main>
+        <footer className="footer">
+          footer
+        </footer>
+      </>
+    
   );
 }
 
-export default App;
+export { App };
